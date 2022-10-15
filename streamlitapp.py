eba7263829ffe20f74f89875749e547b32b81e5c
vatsalsaglani <saglanivatsal@gmail.com>
2022-10-15 20:28:52 +0530

    main streamlit app file

---
+++ streamlitapp.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from utilities import Movies
+from components import card, grid
+
+movObj = Movies()
+
+st.markdown(
+    """<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+""",
+    unsafe_allow_html=True)
+st.header("Prisma Demo")
+movieText = st.text_input("Search Movies")
+
+if movieText:
+    with st.spinner(text="Searching.."):
+        movies = movObj.list_movies_by_title(movieText.rstrip().lstrip())
+        # for ix in range()
+        st.markdown("""<br />""", unsafe_allow_html=True)
+        st.markdown(grid(movies), unsafe_allow_html=True)
