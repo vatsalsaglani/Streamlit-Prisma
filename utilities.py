14f48c32f82dc8ec109188da64eee8099097376b
vatsalsaglani <saglanivatsal@gmail.com>
2022-10-15 21:46:52 +0530

    prisma cleanup

--- utilities.py
+++ utilities.py
@@ -16,6 +16,8 @@ generate_prisma_client()
 try:
     from prisma import Prisma
 except RuntimeError:
+    from prisma_cleanup import cleanup
+    cleanup()
     print(f'GOT RUNTIME ERROR')
     generate_prisma_client()
     from prisma import Prisma
